aa682559423ec751eaa7a97c30322ec807a78b51
afana <afanasiev.iwan@gmail.com>
2025-03-14 19:42:49 +0300

    2

---
+++ src/circle.py
@@ -0,0 +1,15 @@
+from math import pi
+from figure import Figure
+
+class Circle(Figure):
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Circle radius can't be less than 0")
+        self.radius = radius
+
+    def get_area(self):
+        return pi * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * pi * self.radius
+
