35d7cda10f85bf1a5cebdf6e8e8e7f641615f60a
afana <afanasiev.iwan@gmail.com>
2025-03-14 20:00:32 +0300

    2

---
+++ tests/test_triangle.py
@@ -0,0 +1,14 @@
+import pytest
+from src.triangle import Triangle
+
+def test_triangle_area():
+    triangle = Triangle(3, 4, 5)
+    assert triangle.get_area() == 6
+
+def test_triangle_perimeter():
+    triangle = Triangle(3, 4, 5)
+    assert triangle.get_perimeter() == 12
+
+def test_invalid_triangle():
+    with pytest.raises(ValueError):
+        Triangle(1, 2, 3)
